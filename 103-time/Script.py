60b441dd71108af44d3e96a78a2ed4390101f29b
dikyindrah <dikyindrah@gmail.com>
2021-06-19 17:14:05 +0700

    Update 103-time

--- 103-time/Script.py
+++ 103-time/Script.py
@@ -24,7 +24,49 @@ tgl_wkt_saat_ini = tm.ctime(detik)
 print(tgl_wkt_saat_ini)
 
 # sleep()
-
 print('Program dimulai...')
-tm.sleep(2.0)
+# tm.sleep(2.0)
 print('Program selesai...')
+
+
+# locltime
+waktu_lokal = tm.localtime(1545925769)
+
+# Struktur waktu dari kelas struct_tim()
+print(waktu_lokal)
+
+tgl = waktu_lokal.tm_mday
+bln = waktu_lokal.tm_mon
+thn = waktu_lokal.tm_year
+jam = waktu_lokal.tm_hour
+mnt = waktu_lokal.tm_min
+dtk = waktu_lokal.tm_sec
+
+print('{}-{}-{} {}:{}:{}'.format(tgl, bln, thn, jam, mnt, dtk))
+
+# gmtime()
+waktu_utc = tm.gmtime(1545925769)
+
+# Struktur waktu dari kelas struct_tim()
+print(waktu_utc)
+
+tgl = waktu_utc.tm_mday
+bln = waktu_utc.tm_mon
+thn = waktu_utc.tm_year
+jam = waktu_utc.tm_hour
+mnt = waktu_utc.tm_min
+dtk = waktu_utc.tm_sec
+
+print('{}-{}-{} {}:{}:{}'.format(tgl, bln, thn, jam, mnt, dtk))
+
+
+# mktime()
+t = (1999, 11, 27, 0, 0, 0, 0, 0, 0)
+waktu_mk = tm.mktime(t)
+print(waktu_mk)
+
+
+# asctime()
+t = (1999, 11, 27, 0, 0, 0, 0, 0, 0)
+waktu_asc = tm.asctime(t)
+print(waktu_asc)
