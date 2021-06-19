a0b7c4ef808ab034ada532f25ce27eabb7ccb272
dikyindrah <dikyindrah@gmail.com>
2021-06-19 10:32:43 +0700

    Add 103-Time

--- 103-time/Script.py
+++ 103-time/Script.py
@@ -1,9 +1,30 @@
 import time as tm
 
-print('\nProgram mulai...')
+# print('\nProgram mulai...')
 
-for i in range(5):
-    print(i+1)
-    tm.sleep(2.0)
+# for i in range(5):
+#     print(i+1)
+#     tm.sleep(2.0)
 
-print('Program berakhir...')
+# print('Program berakhir...')
+
+# time()
+detik = tm.time()
+print(detik)
+
+# ctime
+# Mengetahui tanggal dan waktu saat pertama kali 
+# ditentukan
+tgl_wkt_pertama = tm.ctime(0.0)
+print(tgl_wkt_pertama)
+
+# Mengetahui tanggal dan waktu saat ini
+detik = tm.time()
+tgl_wkt_saat_ini = tm.ctime(detik)
+print(tgl_wkt_saat_ini)
+
+# sleep()
+
+print('Program dimulai...')
+tm.sleep(2.0)
+print('Program selesai...')
