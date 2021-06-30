bd6197c5842db3151cdda352d3087625606f5211
dikyindrah <dikyindrah@gmail.com>
2021-06-30 21:44:35 +0700

    Update 107-Regular Expression

--- 107-Regular Expression/Script.py
+++ 107-Regular Expression/Script.py
@@ -1,18 +1,17 @@
 import re
 
-pesan = 'ini adalah nomor teleponku 082181177026, tolong disimpan ya!'
+teks = 'wa : 082123456789, ini adalah nomor telepon saya.'
 
 # Mencari nomor telepon dengan cara manual
 nomor_telepon = ''
-for i in range(len(pesan)):
-    teks = pesan[i:i+12]
-    if teks.isdigit():
-        if len(teks) == 12:
-            nomor_telepon = teks
+for i in range(len(teks)):
+    tmp = teks[i:i+12]
+    if tmp.isdigit():
+        if len(tmp) == 12:
+            nomor_telepon = tmp
 
-print('Nomor telepon ditemukan : {}'.format(nomor_telepon))
+print(nomor_telepon)
 
 # Mencari nomor telepon dengan regex
-pola = re.compile(r'\d\d\d\d\d\d\d\d\d\d\d\d')
-nomor_telepon = pola.search(pesan)
-print('Nomor telepon ditemukan : {}'.format(nomor_telepon))
+nomor_telepon = re.search(r'\d\d\d\d\d\d\d\d\d\d\d\d', teks)
+print(nomor_telepon.group())
