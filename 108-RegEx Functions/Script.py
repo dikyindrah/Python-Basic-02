ad9acab9550811b20250809ba093711bfd774cdb
dikyindrah <dikyindrah@gmail.com>
2021-06-30 17:26:19 +0700

    Update 108-RegEx Functions

--- 108-RegEx Functions/Script.py
+++ 108-RegEx Functions/Script.py
@@ -1,60 +1,56 @@
 import re
 
 # findall()
-f_teks = '27 November 1999'
-
-f_pola = '\d+'
-f_hasil = re.findall(f_pola, f_teks)
-print(f_hasil)
+teks = '27 November 1999'
+hasil = re.findall(r'\d+', teks)
+print(hasil)
 
 
 # search()
-sh_teks_1 = 'python adalah bahasa pemrograman yang mudah untuk digunakan'
-sh_teks_2 = 'nomor whatsapp : 082112345678'
+teks = 'javascript, php, pyhton, 12345, go, r'
+hasil = re.search(r'\d\d\d\d\d', teks)
+print(len(teks))
+print(hasil)
 
-sh_pola_1 = 'pemrograman'
-sh_pola_2 = '\d\d\d\d\d\d\d\d\d\d\d\d'
-
-sh_hasil_1 = re.search(sh_pola_1, sh_teks_1)
-sh_hasil_2 = re.search(sh_pola_2, sh_teks_2)
-
-print(sh_hasil_1)
-print(sh_hasil_2)
 
 # group()
-print(sh_hasil_2.group())
+print(hasil.group())
 
 # start(), end(), dan span()
-print('teks = {}'.format(sh_teks_1))
-print('panjang teks = {}'.format(len(sh_teks_1)))
-print('teks yang dicari = {}'.format(sh_hasil_1.group()))
-print('teks {} ditemukan pada rentang indeks {} sampai {}'.format(sh_hasil_1.group(), sh_hasil_1.start(), sh_hasil_1.end()))
-print(sh_hasil_1.span())
+print(hasil.start())
+print(hasil.end())
+print(hasil.span())
 
 # re dan string
-print(sh_hasil_2.re)
-print(sh_hasil_2.string)
+print(hasil.re)
+print(hasil.string)
 
 
 # split()
-st_teks_1 = 'hari ini hujan'
-st_teks_2 = '123-456-789-101'
+teks = 'hari ini hujan lebat'
 
-st_pola_1 = '\s'
-st_pola_2 = r'\D'
-
-st_hasil_1 = re.split(st_pola_1, st_teks_1)
-st_hasil_2 = re.split(st_pola_2, st_teks_2)
-
-
-print(st_hasil_1)
-print(st_hasil_2)
+hasil = re.split(r'\s', teks)
+print(hasil)
 
 # Maxsplit = 1
-st_hasil_3 = re.split(st_pola_2, st_teks_2, 1)
+mx_hasil = re.split(r'\s', teks, 1)
+print(mx_hasil)
 
-# Maxsplit = 2 
-st_hasil_4 = re.split(st_pola_2, st_teks_2, 2)
+# Maxsplit = 2
+mx_hasil = re.split(r'\s', teks, 2)
+print(mx_hasil)
 
-print(st_hasil_3)
-print(st_hasil_4)
+
+# sub()
+teks  = 'saya sedang belajar java'
+hasil = re.sub('java', 'python', teks)
+print(hasil)
+
+teks  = 'java, java, java, java, java, java, java'
+hasil = re.sub('java', 'python', teks, 3)
+print(hasil)
+
+# subn()
+teks  = 'java, go, java, go, java, go, java'
+hasil = re.subn('java', 'python', teks)
+print(hasil)
