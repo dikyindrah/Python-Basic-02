727c5ef965a1cce3f8243377d947b8fd81a6b823
dikyindrah <dikyindrah@gmail.com>
2021-07-15 20:22:46 +0700

    Update 117-Try dan Except

--- 117-Try dan Except/Script.py
+++ 117-Try dan Except/Script.py
@@ -1,3 +1,9 @@
+try:
+    print(x)
+except:
+    print('error!, variabel x belum didefinisikan.')
+
+
 import sys
 
 list_item = ['a', 0, 2]
@@ -13,3 +19,37 @@ for item in list_item:
 
 print(item,'/',item, '=', int(n))
 
+
+try:
+    for i in range(4):
+        print(list_item[i])
+except Exception as e:
+    print(f'Terjadi kesalahan {e.__class__} pada kode program.')
+
+
+try:
+    print(x)
+except NameError as ne:
+    print(ne)
+
+
+try:
+    print(x)
+except ValueError as ve:
+    print(ve)
+except NameError as ne:
+    # Ini adalah penanganan yang sesuai dengan pengecualian
+    print(ne)
+except IndexError as ie:
+    print(ie)
+
+
+list_angka = [1, 2, 3, 4, 5]
+try:
+    for item in list_angka:
+        print(item, end=' ')
+except:
+    e = sys.exc_info()[0]
+    print(e)
+else:
+    print('\nTidak ada kesalahan yang terjadi.')
