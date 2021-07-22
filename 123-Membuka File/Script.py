624449ca0414d09ffe17fde9d13d44a1c84a3ad7
dikyindrah <dikyindrah@gmail.com>
2021-07-22 12:07:17 +0900

    Add 123-Membuka File

---
+++ 123-Membuka File/Script.py
@@ -0,0 +1,7 @@
+# Jika file berada pada direktori 
+# yang sama dengan program
+myfile = open('myfile.txt')
+
+# Jika file berada pada direktori yang 
+# berbeda dengan program
+myfile = open('D:/Python/myfile.txt')
