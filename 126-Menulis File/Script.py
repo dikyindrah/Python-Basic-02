e3362195d7c1c5f7186efc264409820362acaafd
dikyindrah <dikyindrah@gmail.com>
2021-07-23 21:19:44 +0900

    Add 126-Menulis File

---
+++ 126-Menulis File/Script.py
@@ -0,0 +1,42 @@
+# Mode 'w'
+myfile = open('D:/Programming/web/Python/Python-Basic-02/126-Menulis File/myfile.txt', 'w')
+
+# write()
+myfile.write('ini adalah hari yang cerah')
+
+# # writelines()
+# list_string = ['ini ', 'adalah ', 'hari ', 'yang ', 'cerah ']
+# myfile.writelines(list_string)
+
+# # writeable()
+# print(myfile.writable())
+
+
+# Mode 'a'
+# myfile = open('D:/Programming/web/Python/Python-Basic-02/126-Menulis File/myfile.txt', 'a')
+
+# # write()
+# myfile.write('ini adalah hari yang cerah\n')
+# myfile.write('saatnya aku pergi ke sekolah\n')
+
+# # writelines()
+# list_string = ['ini ', 'adalah ', 'hari ', 'yang ', 'cerah \n']
+# # myfile.writelines(list_string)
+# myfile.writelines(list_string)
+
+# # writeable()
+# print(myfile.writable())
+
+
+# Mode 'x'
+# myfile = open('D:/Programming/web/Python/Python-Basic-02/126-Menulis File/myfile2.txt', 'x')
+
+# # write()
+# myfile.write('ini adalah hari yang cerah')
+
+# # writelines()
+# list_string = ['ini ', 'adalah ', 'hari ', 'yang ', 'cerah \n']
+# myfile.writelines(list_string)
+
+# # writeable()
+# print(myfile.writable())
