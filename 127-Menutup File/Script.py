f62fd1403f2b49c59c9137d5b565b83e174d74d3
dikyindrah <dikyindrah@gmail.com>
2021-07-23 23:11:49 +0900

    Add 127-Menutup File

---
+++ 127-Menutup File/Script.py
@@ -0,0 +1,21 @@
+# # Menutup file dengan cara manual
+# myfile = open('D:/Programming/web/Python/Python-Basic-02/127-Menutup File/myfile.txt', 'r')
+# print(myfile.read())
+# myfile.close()
+# print('file berhasil ditutup...')
+
+# Menutup file dengan penanganan pengecualian 
+# (exception handling)
+import sys
+
+try:
+    myfile = open('D:/Programming/web/Python/Python-Basic-02/127-Menutup File/myfile.txt', 'r')
+    print(myfile.read())
+except:
+    print(sys.exc_info()[0])
+finally:
+    try:
+        myfile.close()
+        print('file berhasil ditutup...')
+    except:
+        print(sys.exc_info()[0])
