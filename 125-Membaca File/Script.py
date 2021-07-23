701fa66709d74189352fcbf20e09fe4b632c0d0e
dikyindrah <dikyindrah@gmail.com>
2021-07-23 21:19:12 +0900

    Update 125-Membaca File

--- 125-Membaca File/Script.py
+++ 125-Membaca File/Script.py
@@ -1,25 +1,34 @@
 myfile = open('D:/Programming/web/Python/Python-Basic-02/125-Membaca File/myfile.txt', 'r')
 
-# # Membaca seluruh isi file
+# # Membaca seluruh data file
 # print(myfile.read())
 
 # # Membaca beberapa karakter pada file
 # print(myfile.read(5))
 
 
-# # Membaca isi file baris demi baris
+# # Membaca data file baris demi baris
 # print(myfile.readline())
 # print(myfile.readline())
 # print(myfile.readline())
 
-# # Membaca isi file dengan perulangan 
+# # Membaca data file dengan perulangan 
 # for i in myfile:
 #     print(i, end='')
 
 # # Memeriksa file apakah bisa dibaca atau tidak
 # print(myfile.readable())
 
-# Menyimpan isi file kedalam list
-# Menyimpan seluruh baris isi file kedalam list
-list_seluruh_isi_file = myfile.readlines()
-print(list_seluruh_isi_file)
+# Menyimpan data file kedalam list
+# Menyimpan seluruh baris data file kedalam list
+# list_data = myfile.readlines()
+# print(list_data)
+
+# Mengubah posisi kursor
+myfile.seek(50)
+
+# Mengetahui posisi kursor
+print(myfile.tell())
+
+print('')
+print(myfile.read())
