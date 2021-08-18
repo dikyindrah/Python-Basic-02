7098efb081d26e34eeca995082b0bc23c635987d
dikyindrah <dikyindrah@gmail.com>
2021-08-18 22:12:53 +0900

    Update 127-Menutup File

--- 127-Menutup File/Script.py
+++ 127-Menutup File/Script.py
@@ -4,18 +4,23 @@
 # myfile.close()
 # print('file berhasil ditutup...')
 
-# Menutup file dengan penanganan pengecualian 
-# (exception handling)
-import sys
+# # Menutup file dengan penanganan pengecualian 
+# # (exception handling)
+# import sys
 
-try:
-    myfile = open('D:/Programming/web/Python/Python-Basic-02/127-Menutup File/myfile.txt', 'r')
-    print(myfile.read())
-except:
-    print(sys.exc_info()[0])
-finally:
-    try:
-        myfile.close()
-        print('file berhasil ditutup...')
-    except:
-        print(sys.exc_info()[0])
+# try:
+#     myfile = open('D:/Programming/web/Python/Python-Basic-02/127-Menutup File/myfile.txt', 'r')
+#     print(myfile.read())
+# except:
+#     print(sys.exc_info()[0])
+# finally:
+#     try:
+#         myfile.close()
+#         print('file berhasil ditutup...')
+#     except:
+#         print(sys.exc_info()[0])
+
+# Menutup file dengan keyword with
+with open('D:/Programming/web/Python/Python-Basic-02/127-Menutup File/myfile.txt', 'a') as f:
+    f.write('hello world\n')
+    print('file berhasil ditulis.')
