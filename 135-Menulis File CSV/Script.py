dafb21d0a3742ab4d2bcbffacbae60f16bfd0635
dikyindrah <dikyindrah@gmail.com>
2021-08-20 00:29:07 +0900

    Update 135-Menulis File CSV

--- 135-Menulis File CSV/Script.py
+++ 135-Menulis File CSV/Script.py
@@ -9,11 +9,14 @@ import csv
 #     write.writerow(['3','Tubagus Bagas','Manager'])
 
 # Menulis data dalam beberapa baris menggunakan writerows()
-list_data = [['No','Nama','Profesi'], 
-             ['1','Diky Indra H','Programmer'],
-             ['2','Ardi Wibowo','IT Support'],
-             ['3','Tubagus Bagas','Manager']]
+fields = ['No','Nama','Profesi']
+records = [['1','Diky Indra H','Programmer'],
+           ['2','Ardi Wibowo','IT Support'],
+           ['3','Tubagus Bagas','Manager']]
 
 with open('D:/Programming/web/Python/Python-Basic-02/135-Menulis File CSV/myfile.csv', mode='w', newline='\n') as file:
     write = csv.writer(file)
-    write.writerows(list_data)
+    # Menulis fields
+    write.writerow(fields)
+    # Menulis records
+    write.writerows(records)
