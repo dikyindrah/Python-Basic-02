e6676beaff478323bc96c12fc0b60f2b87ba3a0a
dikyindrah <dikyindrah@gmail.com>
2021-08-20 12:39:45 +0900

    Update 135-Menulis File CSV

--- 135-Menulis File CSV/Script.py
+++ 135-Menulis File CSV/Script.py
@@ -1,6 +1,6 @@
 import csv
 
-# Menulis data dalam satu baris menggunakan writerow()
+# # Menulis data dalam satu baris menggunakan writerow()
 # with open('D:/Programming/web/Python/Python-Basic-02/135-Menulis File CSV/myfile.csv', mode='w', newline='\n') as file:
 #     write = csv.writer(file)
 #     write.writerow(['No','Nama','Profesi'])
@@ -8,15 +8,24 @@ import csv
 #     write.writerow(['2','Ardi Wibowo','IT Support'])
 #     write.writerow(['3','Tubagus Bagas','Manager'])
 
-# Menulis data dalam beberapa baris menggunakan writerows()
-fields = ['No','Nama','Profesi']
-records = [['1','Diky Indra H','Programmer'],
-           ['2','Ardi Wibowo','IT Support'],
-           ['3','Tubagus Bagas','Manager']]
+# # Menulis data dalam beberapa baris menggunakan writerows()
+# fields = ['No','Nama','Profesi']
+# records = [['1','Diky Indra H','Programmer'],
+#            ['2','Ardi Wibowo','IT Support'],
+#            ['3','Tubagus Bagas','Manager']]
 
+# with open('D:/Programming/web/Python/Python-Basic-02/135-Menulis File CSV/myfile.csv', mode='w', newline='\n') as file:
+#     write = csv.writer(file)
+#     # Menulis fields
+#     write.writerow(fields)
+#     # Menulis records
+#     write.writerows(records)
+
+# menulis file csv dari dictionary
 with open('D:/Programming/web/Python/Python-Basic-02/135-Menulis File CSV/myfile.csv', mode='w', newline='\n') as file:
-    write = csv.writer(file)
-    # Menulis fields
-    write.writerow(fields)
-    # Menulis records
-    write.writerows(records)
+    fieldnames = ['id','nama','alamat']
+    write = csv.DictWriter(file, fieldnames=fieldnames)
+
+    write.writeheader()
+    write.writerow({'id':'id001','nama':'diky indra h','alamat':'kedaton'})
+    write.writerow({'id':'id002','nama':'dian budi p','alamat':'way kandis'})
