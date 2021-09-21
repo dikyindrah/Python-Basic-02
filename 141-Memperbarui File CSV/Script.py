49d58d06ce7de0b8ee87a692caa21195619616bf
dikyindrah <dikyindrah@gmail.com>
2021-09-21 23:08:26 +0900

    Update 141-Memperbarui File CSV

--- 141-Memperbarui File CSV/Script.py
+++ 141-Memperbarui File CSV/Script.py
@@ -1,33 +1,33 @@
 import csv
 
 file_path = 'D:/Programming/web/Python/Python-Basic-02/141-Memperbarui File CSV/myfile.csv'
-temp_file = []
+tmp_file = []
 fieldnames = ['no','nama','profesi']
 
 # # Mengubah data pada baris
 # Membaca dan menyalin data pada file ke variable list
 with open(file_path, mode='r', newline='\n') as my_file_csv:
     file = csv.DictReader(my_file_csv)
-    for data in file: temp_file.append(data)
+    for data in file: tmp_file.append(data)
 
 # Menampilkan data dari variable list
 print('\nno \t nama \t\t profesi')
-for data in temp_file: print(f"{data['no']} \t {data['nama']} \t {data['profesi']}")
+for data in tmp_file: print(f"{data['no']} \t {data['nama']} \t {data['profesi']}")
 
 # Mengubah data pada variable list
 no = '2'
 print(f"\nmengubah data no.{no}")
-for i in range(len(temp_file)):
-    if temp_file[i]['no'] == '2':
+for i in range(len(tmp_file)):
+    if tmp_file[i]['no'] == '2':
         nama = str(input('nama \t: '))
         profesi = str(input('profesi : '))
-        temp_file[i]['nama'], temp_file[i]['profesi'] = nama, profesi
+        tmp_file[i]['nama'], tmp_file[i]['profesi'] = nama, profesi
 
 # Menulis data dari variable list ke file
 with open(file_path, mode='w', newline='\n') as my_file_csv:
     file = csv.DictWriter(my_file_csv, fieldnames)
     file.writeheader()
-    file.writerows(temp_file)
+    file.writerows(tmp_file)
 
         
 
