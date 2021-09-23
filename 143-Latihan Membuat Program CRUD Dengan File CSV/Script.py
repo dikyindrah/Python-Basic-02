5064aa3cd19a4507736850eeb513b2f78796c08e
dikyindrah <dikyindrah@gmail.com>
2021-09-23 22:30:18 +0900

    Update 143-Latihan Membuat Program CRUD Dengan File CSV

--- 143-Latihan Membuat Program CRUD Dengan File CSV/Script.py
+++ 143-Latihan Membuat Program CRUD Dengan File CSV/Script.py
@@ -19,7 +19,7 @@ def show_menu():
     elif selected_menu == '2':
         add_contact()
     elif selected_menu == '3':
-        pass
+        update_contact()
     elif selected_menu == '4':
         pass
     elif selected_menu == '5':
@@ -43,37 +43,78 @@ def back_to_menu():
     clear_screen()
     show_menu()
 
-def show_contact():
-    clear_screen()
+def add_contact_to_temporary():
     contact = []
-    with open(my_file_path, mode='r', encoding='UTF-8', newline='\n') as my_file_csv:
-        file = csv.DictReader(my_file_csv)
+    with open(my_file_path, mode='r', encoding='UTF-8', newline='\n') as my_csv_file:
+        file = csv.DictReader(my_csv_file)
         for data in file:
             contact.append(data)
     
-    print(f"{'='*5}Lihat Daftar Kontak{'='*5}")
-    field = list(contact[0].keys())
-    print(f"{field[0]}\t{field[1]}\t\t{field[2]}")
-    for data in contact:
-        print(f"{data['No']}\t{data['Nama']}\t{data['Kontak']}")
+    return contact
+
+def show_contact_from_temporary():
+    all_contact = add_contact_to_temporary()
+    field = list(all_contact[0].keys())
     
+    print(f"{field[0]}\t{field[1]}\t\t{field[2]}")
+    for contact in all_contact:
+        print(f"{contact['No']}\t{contact['Nama']}\t{contact['Kontak']}")
+
+def show_contact():
+    clear_screen()
+
+    print(f"{'='*5}Lihat Daftar Kontak{'='*5}")
+    show_contact_from_temporary()
+
     back_to_menu()
 
 def add_contact():
     clear_screen()
+    print(f"{'='*5}Tambah Kontak{'='*5}")
     with open(my_file_path, mode='a', encoding='UTF-8', newline='\n') as my_file_csv:
         fieldnames = ['No','Nama','Kontak']
         file = csv.DictWriter(my_file_csv, fieldnames=fieldnames)
         
-        No = str(input('No : '))
-        Nama = str(input('Nama : '))
-        Kontak = str(input('Kontak : '))
+        No = str(input('No \t: '))
+        Nama = str(input('Nama \t: '))
+        Kontak = str(input('Kontak \t: '))
 
         file.writerow({'No':No,'Nama':Nama,'Kontak':Kontak})
         print('\nKontak berhasil ditambahkan.')
     
     back_to_menu()
 
+def update_contact():
+    clear_screen()
+    all_contact = add_contact_to_temporary()
+    
+    print(f"{'='*5}Ubah Kontak{'='*5}")
+    show_contact_from_temporary()
+
+    No = str(input('\nPilih nomor data yang ingin anda ubah : '))
+    for i in range(len(all_contact)):
+        if all_contact[i]['No'] == No:
+            clear_screen()
+            print(f"{'='*5}Ubah Kontak{'='*5}")
+            field = list(all_contact[i].keys())
+            print(f"{field[0]}\t{field[1]}\t\t{field[2]}")
+            print(f"{all_contact[i]['No']}\t{all_contact[i]['Nama']}\t{all_contact[i]['Kontak']}")
+
+            Nama = str(input('\nNama \t: '))
+            Kontak = str(input('Kontak \t: '))
+            all_contact[i]['Nama'] = Nama
+            all_contact[i]['Kontak'] = Kontak
+    
+    with open(my_file_path, mode='w', encoding='UTF-8', newline='\n') as my_file_csv:
+        fieldnames = ['No', 'Nama', 'Kontak']
+        file = csv.DictWriter(my_file_csv, fieldnames=fieldnames)
+        file.writeheader()
+        file.writerows(all_contact)
+    
+    back_to_menu()
+
+def delete_contact():
+    pass
 
 
 show_menu()
