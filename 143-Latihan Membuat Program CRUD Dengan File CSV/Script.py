72029fe0a3eeed3bb05df9f9c30f3f202886a318
dikyindrah <dikyindrah@gmail.com>
2021-09-27 11:52:33 +0900

    Update 143-Latihan Membuat Program CRUD Dengan File CSV

--- 143-Latihan Membuat Program CRUD Dengan File CSV/Script.py
+++ 143-Latihan Membuat Program CRUD Dengan File CSV/Script.py
@@ -5,7 +5,7 @@ import platform
 my_file_path = 'D:/Programming/web/Python/Python-Basic-02/143-Latihan Membuat Program CRUD Dengan File CSV/myfile.csv'
 
 def show_menu():
-    print(f"{'='*5}Buku Telepon{'='*5}")
+    print(f"{'='*10}Buku Telepon{'='*10}")
     print('[1] Lihat Daftar Kontak')
     print('[2] Tambah Kontak')
     print('[3] Ubah Kontak')
@@ -76,7 +76,7 @@ def add_contact():
     clear_screen()
 
     print(f"{'='*10}Tambah Kontak{'='*10}")
-    Nama = str(input('Nama \t: '))
+    nama = str(input('Nama \t: '))
     Kontak = str(input('Kontak \t: '))
     
     add = str(input('\nTambahkan ke daftar kontak? [y/t]: '))
@@ -85,7 +85,7 @@ def add_contact():
         with open(my_file_path, mode='a', encoding='UTF-8', newline='\n') as my_file_csv:
             fieldnames = ['Nama','Kontak']
             file = csv.DictWriter(my_file_csv, fieldnames=fieldnames)
-            file.writerow({'Nama':Nama,'Kontak':Kontak})
+            file.writerow({'Nama':nama,'Kontak':Kontak})
             print('\nKontak telah ditambahkan.')
         back_to_menu()
     elif add.lower() == 't':
@@ -99,27 +99,27 @@ def update_contact():
     clear_screen()
     tmp_contact = add_contact_to_temporary()
     
-    print(f"{'='*5}Ubah Kontak{'='*5}")
+    print(f"{'='*10}Ubah Kontak{'='*10}")
     show_contact_from_temporary()
 
-    No = int(input('\nPilih \'No\' untuk mengubah kontak: '))
+    no = int(input('\nPilih \'No\' untuk mengubah kontak: '))
     data_found = False
     for i in range(len(tmp_contact)):
-        if i == No-1:
+        if i == no-1:
             data_found = True
             break;
-    
+        
     if data_found == True:
         clear_screen()
         print(f"{'='*5}Ubah Kontak{'='*5}")
         field = list(tmp_contact[0].keys())
         print(f"No\t{field[0]}\t\t{field[1]}")
-        print(f"{No}\t{tmp_contact[No-1]['Nama']}\t{tmp_contact[No-1]['Kontak']}")
+        print(f"{no}\t{tmp_contact[no-1]['Nama']}\t{tmp_contact[no-1]['Kontak']}")
 
-        Nama = str(input('\nNama \t: '))
-        Kontak = str(input('Kontak \t: '))
-        tmp_contact[No-1]['Nama'] = Nama
-        tmp_contact[No-1]['Kontak'] = Kontak
+        nama = str(input('\nNama \t: '))
+        kontak = str(input('Kontak \t: '))
+        tmp_contact[no-1]['Nama'] = nama
+        tmp_contact[no-1]['Kontak'] = kontak
 
         with open(my_file_path, mode='w', encoding='UTF-8', newline='\n') as my_file_csv:
             fieldnames = ['Nama', 'Kontak']
@@ -134,58 +134,109 @@ def update_contact():
 def delete_contact():
     clear_screen()
     tmp_contact = add_contact_to_temporary()
-
+    
+    print(f"{'='*10}Hapus Kontak{'='*10}")    
     show_contact_from_temporary()
-    No = int(input('\nPilih \'No\' untuk menghapus kontak: '))
+    no = int(input('\nPilih \'No\' untuk menghapus kontak: '))
     data_found = False
     for i in range(len(tmp_contact)):
-        if i == No-1:
+        if i == no-1:
             data_found = True
             break;
-    
     if data_found == True:
-        tmp_contact.remove(tmp_contact[No-1])
+        tmp_contact.remove(tmp_contact[no-1])
         with open(my_file_path, mode='w', encoding='UTF-8', newline='\n') as my_file_csv:
             fieldnames = ['Nama','Kontak']
             file = csv.DictWriter(my_file_csv, fieldnames=fieldnames)
             file.writeheader()
             file.writerows(tmp_contact)
-        print('Kontak berhasil dihapus.')
+        print('\nKontak berhasil dihapus.')
     else:
-        print('Kontak tidak ditemukan.')
+        print('\nKontak tidak ditemukan.')
     
     back_to_menu()
 
 def search_contact():
     clear_screen()
-    all_contact = add_contact_to_temporary()
+    tmp_contact = add_contact_to_temporary()
 
-    show_contact_from_temporary()
+    print(f"{'='*10}Cari Kontak{'='*10}")
+    print('[1] No')
+    print('[2] Nama')
+    print('[3] Kontak')
 
-    search_data = str(input('\nCari data : '))
-    total_data = 0
-    index_data = []
-    for i in range(len(all_contact)):
-        if (all_contact[i]['No'] == search_data 
-        or all_contact[i]['Nama'] == search_data 
-        or all_contact[i]['Kontak'] == search_data):
-            total_data = total_data + 1
-            index_data.append(i)
-
-    clear_screen()
-    if total_data != 0:
-        print(f"Total data yang ditemukan {total_data}\n")
-        field = list(all_contact[0].keys())
-        print(f"{field[0]}\t{field[1]}\t\t{field[2]}")
-        for i in range(len(index_data)):
-            index = int(index_data[i])
-            print(f"{all_contact[index]['No']}\t{all_contact[index]['Nama']}\t{all_contact[index]['Kontak']}")
+    selected_menu = str(input('\nPilih \'No\' untuk memilih jenis pencarian: '))
+    if selected_menu == '1':
+        clear_screen()
+        print(f"{'='*10}Cari Kontak{'='*10}")
+        show_contact_from_temporary()
+        no = int(input('\nPilih \'No\' untuk mencari kontak: '))
+        kontak_found = False
+        for i in range(len(tmp_contact)):
+            if i == no-1:
+                kontak_found = True
+                break;
+        
+        clear_screen()
+        print(f"{'='*10}Cari Kontak{'='*10}")
+        if kontak_found == True:
+            print('Kontak ditemukan.\n')
+            field = list(tmp_contact[0].keys())
+            print(f"No\t{field[0]}\t\t{field[1]}")
+            print(f"{no}\t{tmp_contact[no-1]['Nama']}\t{tmp_contact[no-1]['Kontak']}")
+        else:
+            print('Kontak tidak ditemukan!')
+    elif selected_menu == '2':
+        clear_screen()
+        print(f"{'='*10}Cari Kontak{'='*10}")
+        show_contact_from_temporary()
+        nama = str(input('\nPilih \'Nama\' untuk mencari kontak: '))
+        kontak_found = False
+        index_kontak = []
+        for i in range(len(tmp_contact)):
+            if nama in tmp_contact[i]['Nama']:
+                kontak_found = True
+                index_kontak.append(i)
+        
+        clear_screen()
+        print(f"{'='*10}Cari Kontak{'='*10}")
+        if kontak_found == True:
+            print(f"Total kontak ditemukan {len(index_kontak)}\n")
+            field = list(tmp_contact[0].keys())
+            print(f"No\t{field[0]}\t\t{field[1]}")
+            for i in range(len(index_kontak)):
+                print(f"{index_kontak[i]+1}\t{tmp_contact[index_kontak[i]]['Nama']}\t{tmp_contact[index_kontak[i]]['Kontak']}")
+        else:
+            print('Kontak tidak ditemukan.')
+    elif selected_menu == '3':
+        clear_screen()
+        print(f"{'='*10}Cari Kontak{'='*10}")
+        show_contact_from_temporary()
+        kontak = str(input('\nPilih \'Kontak\' untuk mencari kontak: '))
+        kontak_found = False
+        index_kontak = []
+        for i in range(len(tmp_contact)):
+            if kontak in tmp_contact[i]['Kontak']:
+                kontak_found = True
+                index_kontak.append(i)
+        
+        clear_screen()
+        print(f"{'='*10}Cari Kontak{'='*10}")
+        if kontak_found == True:
+            print(f"Total kontak ditemukan {len(index_kontak)}\n")
+            field = list(tmp_contact[0].keys())
+            print(f"No\t{field[0]}\t\t{field[1]}")
+            for i in range(len(index_kontak)):
+                print(f"{index_kontak[i]+1}\t{tmp_contact[index_kontak[i]]['Nama']}\t{tmp_contact[index_kontak[i]]['Kontak']}")
+        else:
+            print('Kontak tidak ditemukan.')
     else:
-        print('Data tidak ditemukan!')
+        print('\nPerintah tidak diketahui!')
     
     back_to_menu()
 
 def close_menu():
+    clear_screen()
     os.system('exit')
 
 if __name__ == '__main__':
