c52f7b761b5fd20edd22a0a0b4fb88c33bfb5a94
dikyindrah <dikyindrah@gmail.com>
2021-10-20 23:12:54 +0700

    Add 157-Membuat Database Dengan mysql-connector

---
+++ 157-Membuat Database Dengan mysql-connector/Script.py
@@ -0,0 +1,20 @@
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    with mysql.connector.connect(
+        host = 'localhost',
+        username = 'root',
+        password = ''
+    ) as koneksi:
+        create_db_query = 'CREATE DATABASE db_profesi'
+        show_db_query = 'SHOW DATABASES'
+        with koneksi.cursor() as cursor:
+            cursor.execute(create_db_query)
+            print('Database berhasil dibuat\n')
+            print('Daftar database:\n')
+            cursor.execute(show_db_query)
+            for db in cursor:
+                print(db)
+except Error as e:
+    print(f"Database gagal dibuat, Error: {e}")
